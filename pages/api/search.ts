c26e400a528cbf8edf359ad3cc8db7cb1f15343d
Kevin <kevnguyenp@gmail.com>
2023-06-05 15:56:24 -0400

    Do not return user from recent

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,13 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import UserModel, { UserDocument } from "../../src/models/user.model";
+import cookie from 'cookie'
+import { validateToken } from "../../src/service/user.service";
 
 // TODO: Properly implement searching, just returns all users at the moment
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const { token } = cookie.parse(req.headers.cookie || '')
+    if (!token) res.status(400).send("Invalid Request: Token Required")
+
     if (req.method == "GET") {
         // 1. Manual search by name
         const { search_all } = req.query
         if (search_all === "true") {
-            return demo_getRecentUsers(req, res)
+            return demo_getRecentUsers(req, res, token)
         }
         res.status(200).send({ test: true })
         // 2. Search by token > convo contents
@@ -15,8 +21,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 }
 
 // Returns 5 most recently registered users
-async function demo_getRecentUsers(req: NextApiRequest, res: NextApiResponse) {
-    const recent_users = await UserModel.find().sort({ createdAt: -1 }).limit(5) as UserDocument[]
+async function demo_getRecentUsers(req: NextApiRequest, res: NextApiResponse, token: string) {
+
+    const user = await validateToken(token)
+    if (!user) return res.status(400).send("Invalid Request: Invalid Token")
+
+    const recent_users = await UserModel.find({ _id: { $ne: user._id } }).sort({ createdAt: -1 }).limit(10) as UserDocument[]
 
     if (!recent_users)
         return res.status(404).send([])
