5bc0d0df4a3e8cf464ae0e60dc89797362866ddf
Kevin <kevnguyenp@gmail.com>
2023-06-19 13:46:42 -0400

    fix 3?

--- pages/api/conv.ts
+++ pages/api/conv.ts
@@ -96,12 +96,14 @@ async function appendMessageToConvo(req: NextApiRequest, res: NextApiResponse, t
     const socket = io(SOCKETIO_URI, { path: "/socketio/socket.io" })
 
     socket.on("connect", () => {
+        logger.info(`SOCKET CONNECTED (backend): ${socket}`)
         socket.emit("joinRoom", req.body.convo_id)
+        logger.info(`${socket} (backend) joined room: ${req.body.convo_id}`)
         socket.emit("roomMessage", { convo_id: req.body.convo_id, content: JSON.stringify(message) })
+        logger.info(`${socket} (backend) send message: ${JSON.stringify(message)} to room: ${req.body.convo_id}`)
         socket.emit("leaveRoom", req.body.convo_id)
         socket.disconnect()
     })
-
 
     return res.status(200).json(message)
 }
