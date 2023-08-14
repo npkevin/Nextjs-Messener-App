2e109c4f2c588980e026009cc9ecd0147db5820f
Kevin <kevnguyenp@gmail.com>
2023-08-13 21:19:23 -0400

    tailwind setup & prettier format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,87 @@
-import type { NextPage } from 'next'
-import { createContext, useEffect, useState } from 'react'
-import cookies from 'js-cookie'
+import type { NextPage } from "next";
+import { createContext, useEffect, useState } from "react";
+import cookies from "js-cookie";
 
-import SideMenu from '../src/components/SideMenu/SideMenu'
-import MessengerView from '../src/components/Messenger/MessengerView'
-import styles from '../styles/index.module.css'
+import SideMenu from "../src/components/SideMenu/SideMenu";
+import MessengerView from "../src/components/Messenger/MessengerView";
 
-import { Types } from 'mongoose'
-import { MessageDocument } from '../src/models/message.model'
+import { Types } from "mongoose";
+import { MessageDocument } from "../src/models/message.model";
 
 interface IdefaultState {
-    validToken: boolean
+    validToken: boolean;
     user?: {
         name: {
-            first: string,
-            last: string,
-            middle: string,
-        }
-    },
+            first: string;
+            last: string;
+            middle: string;
+        };
+    };
     convo?: {
-        id: Types.ObjectId,
+        id: Types.ObjectId;
         user: {
-            id: Types.ObjectId,
+            id: Types.ObjectId;
             name: {
-                first: string,
-                last: string,
-                middle: string,
-            }
-        },
-        messages_history: MessageDocument[]
-    }
+                first: string;
+                last: string;
+                middle: string;
+            };
+        };
+        messages_history: MessageDocument[];
+    };
 }
 const defaultState: IdefaultState = {
     validToken: false,
     user: undefined,
     convo: undefined,
+};
+
+interface IAppState {
+    state: IdefaultState;
+    setState: (new_state: {}) => void;
 }
-
-interface IAppState { state: IdefaultState, setState: (new_state: {}) => void }
-export const AppStateCtx = createContext<IAppState>({ state: defaultState, setState: () => undefined })
-
+export const AppStateCtx = createContext<IAppState>({
+    state: defaultState,
+    setState: () => undefined,
+});
 
 const Home: NextPage = (): JSX.Element => {
-    const [state, _setState] = useState(defaultState)
-    const setState = (new_state: {}) => _setState(prev_state => ({ ...prev_state, ...new_state }))
+    const [state, _setState] = useState(defaultState);
+    const setState = (new_state: {}) =>
+        _setState((prev_state) => ({ ...prev_state, ...new_state }));
 
     useEffect(() => {
         const checkCookieToken = async () => {
-            const response = await fetch(`/api/auth`)
+            const response = await fetch(`/api/auth`);
 
-            if (!response.ok) cookies.remove("token")
+            if (!response.ok) cookies.remove("token");
             else {
-                const response_json = await response.json()
-                setState({ validToken: true, user: { name: response_json.name } })
+                const response_json = await response.json();
+                setState({
+                    validToken: true,
+                    user: { name: response_json.name },
+                });
             }
-        }
-        const token = cookies.get("token")
+        };
+        const token = cookies.get("token");
         if (token && !state.validToken) {
-            checkCookieToken()
+            checkCookieToken();
         }
 
         return () => {
             // cleanup
-        }
-    }, [state.validToken])
+        };
+    }, [state.validToken]);
 
     return (
         <AppStateCtx.Provider value={{ state, setState }}>
-            <div className={styles.container}>
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            <div className="flex flex-row">
                 <SideMenu />
-                <MessengerView />
+                {/* <MessengerView /> */}
             </div>
         </AppStateCtx.Provider>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
--- src/components/SideMenu/Authentication.tsx
+++ src/components/SideMenu/Authentication.tsx
@@ -1,41 +1,49 @@
-import { useContext, useEffect, useState } from 'react'
-import cookies from 'js-cookie'
+import {
+    Dispatch,
+    SetStateAction,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
+import { AppStateCtx } from "../../../pages";
+import cookies from "js-cookie";
 
-import styles from '../../../styles/Authentication.module.css'
-import { AppStateCtx } from '../../../pages'
-
+import { TextInput, Button } from "../UI/Input";
 
 const Authentication = (): JSX.Element => {
+    const { state, setState } = useContext(AppStateCtx);
 
-    const { state, setState } = useContext(AppStateCtx)
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [passwordConfirmation, setPasswordConfirmation] =
+        useState<string>("");
 
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const [passwordConfirmation, setPasswordConfirmation] = useState<string>("")
+    const [firstname, setFirstname] = useState<string>("");
+    const [middlename, setMiddlename] = useState<string>("");
+    const [lastname, setLastname] = useState<string>("");
 
-    const [firstname, setFirstname] = useState<string>("")
-    const [middlename, setMiddlename] = useState<string>("")
-    const [lastname, setLastname] = useState<string>("")
+    const [btnSignInDisable, setBtnSignInDisable] = useState<boolean>(false);
+    const [btnSignUpDisable, setBtnSignUpDisable] = useState<boolean>(true);
 
-    const [btnSignInDisable, setBtnSignInDisable] = useState<boolean>(false)
-    const [btnSignUpDisable, setBtnSignUpDisable] = useState<boolean>(true)
-
-    const [warning, setWarning] = useState<{ show: boolean, message: string }>({ show: false, message: "" })
+    const [warning, setWarning] = useState<{ show: boolean; message: string }>({
+        show: false,
+        message: "",
+    });
 
     useEffect(() => {
-        const credInvalid = (email === "") || (password === "")
-        setBtnSignInDisable(credInvalid || firstname !== "" || lastname !== "")
-    }, [email, password, firstname, lastname])
+        const credInvalid = email === "" || password === "";
+        setBtnSignInDisable(credInvalid || firstname !== "" || lastname !== "");
+    }, [email, password, firstname, lastname]);
 
     const SignUp = async () => {
         if (btnSignUpDisable === true) {
-            setBtnSignUpDisable(false)
-            return
+            setBtnSignUpDisable(false);
+            return;
         }
         const response: Response = await fetch(`/api/auth`, {
             method: "POST",
             headers: {
-                "content-type": "application/json"
+                "content-type": "application/json",
             },
             body: JSON.stringify({
                 email: email,
@@ -45,116 +53,136 @@ const Authentication = (): JSX.Element => {
                     first: firstname,
                     middle: middlename,
                     last: lastname,
-                }
-            })
-        })
-        const response_json = await response.json()
+                },
+            }),
+        });
+        const response_json = await response.json();
         if (response.ok) {
-            cookies.set("token", response_json.session, { sameSite: "Strict" })
-            setState({ validToken: true, user: { name: response_json.name } })
+            cookies.set("token", response_json.session, { sameSite: "Strict" });
+            setState({ validToken: true, user: { name: response_json.name } });
         } else {
             // assuming we only get zod parsing errors
-            let message_string = ""
-            response_json.forEach((error: any) => { message_string += "• " + error.message + "\n" });
-            setWarning({ show: true, message: message_string }) // SafeParseError.error.issues[]
+            let message_string = "";
+            response_json.forEach((error: any) => {
+                message_string += "• " + error.message + "\n";
+            });
+            setWarning({ show: true, message: message_string }); // SafeParseError.error.issues[]
         }
-    }
+    };
 
     const SignIn = async () => {
         const response: Response = await fetch(`/api/auth`, {
             method: "POST",
             headers: {
-                "content-type": "application/json"
+                "content-type": "application/json",
             },
             body: JSON.stringify({
                 signin: true,
                 email: email,
-                password: password
-            })
-        })
-        const response_json = await response.json()
+                password: password,
+            }),
+        });
+        const response_json = await response.json();
         if (response.ok) {
-            cookies.set("token", response_json.session, { sameSite: "Strict" })
-            setState({ validToken: true, user: { name: response_json.name } })
+            cookies.set("token", response_json.session, { sameSite: "Strict" });
+            setWarning({ show: false, message: warning.message });
+            setState({ validToken: true, user: { name: response_json.name } });
         } else {
             // assuming we only get zod parsing errors
-            let message_string = ""
-            response_json.forEach((error: any) => { message_string += "• " + error.message + "\n" });
-            setWarning({ show: true, message: message_string }) // SafeParseError.error.issues[]
+            let message_string = "";
+            response_json.forEach((error: any) => {
+                message_string += "• " + error.message + "\n";
+            });
+            setWarning({ show: true, message: message_string }); // SafeParseError.error.issues[]
         }
-    }
+    };
 
     const SignOut = async () => {
         // clear fields
-        setEmail("")
-        setPassword("")
-        setPasswordConfirmation("")
-        setFirstname("")
-        setMiddlename("")
-        setLastname("")
-        setBtnSignUpDisable(true)
+        setEmail("");
+        setPassword("");
+        setPasswordConfirmation("");
+        setFirstname("");
+        setMiddlename("");
+        setLastname("");
+        setBtnSignUpDisable(true);
 
-        await fetch(`/api/auth`, { method: "DELETE" })
-        cookies.remove('token')
-        setState({ validToken: false, user: undefined, convo: undefined })
-    }
+        await fetch(`/api/auth`, { method: "DELETE" });
+        cookies.remove("token");
+        setState({ validToken: false, user: undefined, convo: undefined });
+    };
 
-    return (!state.validToken ?
-        <div className={styles.container}>
-            <div className={styles.signUp + (btnSignUpDisable ? "" : " " + styles.signUp__unhide)}>
-                <input
-                    type="text"
-                    placeholder='First name'
-                    value={firstname}
-                    onChange={event => setFirstname(event.target.value)}
+    const signUpHide = btnSignUpDisable
+        ? "pointer-events-none max-h-0 select-none opacity-0 "
+        : "ease-in duration-200 max-h-40 ";
+
+    const warningHide = warning.show
+        ? "ease-in duration-200 opacity-100"
+        : "opacity-0 select-none pointer-events-none";
+
+    return !state.validToken ? (
+        <div className="flex flex-col">
+            <div className={"flex flex-col " + signUpHide}>
+                <TextInput
+                    placeholder="First name"
+                    state={[firstname, setFirstname]}
                 />
-                <input
-                    type="text"
-                    placeholder='Middle name (optional)'
-                    value={middlename}
-                    onChange={event => setMiddlename(event.target.value)}
+                <TextInput
+                    placeholder="Middle name (optional)"
+                    state={[middlename, setMiddlename]}
                 />
-                <input
-                    type="text"
-                    placeholder='Last name'
-                    value={lastname}
-                    onChange={event => setLastname(event.target.value)}
+                <TextInput
+                    placeholder="Last name"
+                    state={[lastname, setLastname]}
                 />
             </div>
-            <div>
-                <input
+            <div className="flex flex-col">
+                <TextInput
                     type="email"
-                    placeholder='Email'
-                    value={email}
-                    onChange={event => setEmail(event.target.value)}
+                    placeholder="Email"
+                    state={[email, setEmail]}
                 />
-                <input
+                <TextInput
                     type="password"
-                    placeholder='Password'
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
+                    placeholder="Password"
+                    state={[password, setPassword]}
                 />
             </div>
-            <div className={styles.signUp + (btnSignUpDisable ? "" : " " + styles.signUp__unhide)}>
-                <input
+            <div className={"flex flex-col " + signUpHide}>
+                <TextInput
                     type="password"
-                    placeholder='Confirm Password'
-                    value={passwordConfirmation}
-                    onChange={event => setPasswordConfirmation(event.target.value)}
+                    placeholder="Confirm Password"
+                    state={[passwordConfirmation, setPasswordConfirmation]}
                 />
             </div>
-            <div className={styles.signOptions}>
-                <button onClick={SignUp}>Sign Up</button>
-                <button onClick={SignIn} disabled={btnSignInDisable}>Login</button>
+            <div className="flex flex-row justify-between">
+                <Button onClick={SignUp} value="Sign Up" />
+                <Button
+                    onClick={SignIn}
+                    value="Login"
+                    disabled={btnSignInDisable}
+                />
             </div>
-            <div className={styles.signWarning + (warning.show ? " " + styles.signWarning__unhide : "")}>
-                <button onClick={() => setWarning({ show: false, message: "" })}>x</button>
-                <span style={{ whiteSpace: "pre-line" }}>{warning.message}</span>
+            <div
+                className={
+                    "flex flex-col w-full mt-4 p-2 rounded-lg bg-amber-300 " +
+                    warningHide
+                }
+            >
+                <button
+                    className="self-end"
+                    onClick={() =>
+                        setWarning({ show: false, message: warning.message })
+                    }
+                >
+                    🞬
+                </button>
+                <span>{warning.message}</span>
             </div>
         </div>
-        :
+    ) : (
         <button onClick={SignOut}>Sign Out</button>
-    )
-}
+    );
+};
 
-export default Authentication
+export default Authentication;
